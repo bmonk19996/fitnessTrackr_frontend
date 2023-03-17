b34aca56126e32c2e95b8ff8a96fd2ffde338d9f
UpcomingWhale <javascriptcodingguru@gmail.com>
2023-03-17 12:01:07 -0400

    add api functions

--- src/components/API-adapt/index.js
+++ src/components/API-adapt/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = "localhost:3000/api/";
+const BASE_URL = "localhost:3000/api";
 
 function makeHeaders(token){
     const header = { "Content-Type": "application/json" };
@@ -8,17 +8,209 @@ function makeHeaders(token){
     return header;
 }
 
-const getRoutines = async () => {
-    try {
-      const response = await fetch(`${BASE_URL}routines`, {
-        method: "GET",
-        headers: makeHeaders(),
+  const register = async (username, password)=>
+  {
+    try
+    {
+      const response = await fetch(`${BASE_URL}/users/register`, {
+        method:"POST",
+        headers:makeHeaders(),
+        body:{
+          username:username,
+          password:password
+        }
       });
       const result = await response.json();
-      console.log(result)
       return result;
-    } catch (error) {
-      console.log(error);
     }
-  };
-getRoutines()
+    catch(e)
+    {
+      throw e;
+    }
+  }
+
+  const login = async (username, password) =>{
+
+    try{
+      const response = await fetch(`${BASE_URL}/users/login`,{
+        method:"POST",
+        headers: makeHeaders(),
+        body:{
+          username:username,
+          password:password
+        }
+      });
+      const result = await response.json();
+      return result;
+    }catch(error)
+    {
+      throw error;
+    }
+  }
+
+  const getMyUser = async (token) =>
+  {
+    try{
+      const response = await fetch(`${BASE_URL}/users/me`, {
+        method:"GET",
+        headers:makeHeaders(token),
+      });
+      const result = await response.json();
+      return result;
+    }catch(e){
+      throw e;
+    }
+  }
+
+  const getMyRoutines = async (token, username) =>
+  {
+    try
+    {
+      const response = await fetch(`${BASE_URL}/users/${username}/routines`,{
+        method:"GET",
+        headers:makeHeaders(token),
+      });
+      const result = await response.json();
+      return result;
+    }
+    catch(e)
+    {
+      throw e;
+    }
+  }
+
+  const getUserPublicRoutines = async (username) =>
+  {
+    try
+    {
+      const response = await fetch(`${BASE_URL}/users/${username}/routines`, {
+        method:"GET",
+        headers:makeHeaders(),
+      });
+      const result = await response.json();
+      return result;
+    }
+    catch(e)
+    {
+      throw e;
+    }
+  }
+  
+const getRoutines = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/routines`, {
+      method: "GET",
+      headers: makeHeaders(),
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+const makeRoutine = async(token, fields) =>
+{
+  try{
+    const response = await fetch(`${BASE_URL}/routines`, {
+      method: "POST",
+      headers: makeHeaders(token),
+      body:{ 
+        ...fields
+      }
+    });
+    const result = response.json();
+    return result;
+  }
+  catch(e)
+  {
+    throw e;
+  }
+}
+const updateRoutine = async(token, routineId, fields ) =>
+{
+  try
+  {
+    const response = await fetch(`${BASE_URL}/${routineId}`,  {
+      method:"POST",
+      headers: makeHeaders(token),
+      body:{
+        ...fields
+      }
+    });
+    const result = await response.json();
+    return result;
+  }catch(e)
+  {
+    throw e;
+  }
+}
+const deleteRoutine = async(token, routineId) =>
+{
+  try {
+    const response = await fetch(`${BASE_URL}/${routineId}`, {
+      method:"DELETE",
+      headers:makeHeaders(token),
+      body:{
+        routineId: routineId
+      }
+    });
+    const result = await response.json();
+    return result;
+  }
+  catch(e)
+  {
+    throw e;
+  }
+}
+
+const addActivityToRoutine = async (token, routineId, activityId)=>
+{
+  try
+  {
+    const response = await fetch(`${BASE_URL}/${routineId}`,{
+      method:"POST",
+      headers:makeHeaders(token),
+      body:{
+        routineId:routineId,
+        activityId:activityId
+      }
+    });
+    const result = await response.json();
+    return result;
+  }catch(e)
+  {
+    throw e;
+  }
+}
+
+const getAllActivities = async () =>
+{
+  try
+  {
+    const response = await fetch(`${BASE_URL}/activities`, {
+      method:"GET",
+      header:makeHeaders()
+    });
+    const result = await response.json();
+    return result;
+  }
+  catch(e)
+  {
+    throw e;
+  }
+}
+
+const getRoutinesByActivity = async(activityId) =>
+{
+  try{
+    const response = await fetch(`${BASE_URL}/${activityId}/routines`,{
+      method:"GET",
+      headers:makeHeaders(),
+    });
+    const result = await response.json();
+    return result;
+  }catch(e)
+  {
+    throw e;
+  }
+}
