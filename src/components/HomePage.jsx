44c153946217974861660c692df973478c15d67d
MonkComputer <bmonk1996@yahoo.com>
2023-03-24 12:09:47 -0500

    small change

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const HomePage = (props) => {
   return (
     <div id="homePage"> 
-    <h1>Select A Tab to view</h1> 
+    <h1>Select a tab to view</h1> 
     </div>
   );
 };
